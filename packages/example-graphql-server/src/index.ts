2510d790d9634c655461020c2220add44af482fe
Admin <enfros2000@gmail.com>
2021-03-05 17:20:07 +0300

    Fix: Workspace names Fix: TSConfig Fix: Server init Feat: createGraphQLWS

--- packages/example-graphql-server/src/index.ts
+++ packages/example-graphql-server/src/index.ts
@@ -1,54 +1,56 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-require('dotenv').config();
-import { ApolloServer } from 'apollo-server-express';
-import { buildGraphbackAPI } from 'graphback';
-import { createMongoDbProvider } from '@graphback/runtime-mongo';
-import cors from 'cors';
-import express from 'express';
-import http from 'http';
-import { loadConfigSync } from 'graphql-config';
-import { connectDB } from './db';
-import { noteResolvers } from './resolvers/noteResolvers';
-
+require('dotenv').config()
+import { ApolloServer, ApolloServerExpressConfig } from 'apollo-server-express'
+import { buildGraphbackAPI } from 'graphback'
+import { createMongoDbProvider } from '@graphback/runtime-mongo'
+import cors from 'cors'
+import express from 'express'
+import http from 'http'
+import { loadConfigSync } from 'graphql-config'
+import { connectDB } from './db'
+import { noteResolvers } from './resolvers/noteResolvers'
+import { createGraphQLWS } from '../../cactus-sync-server/lib'
 async function start() {
-  const app = express();
+  const app = express()
 
-  app.use(cors());
+  app.use(cors())
 
-  const graphbackExtension = 'graphback';
+  const graphbackExtension = 'graphback'
   const config = loadConfigSync({
     extensions: [
       () => ({
-        name: graphbackExtension
-      })
-    ]
-  });
+        name: graphbackExtension,
+      }),
+    ],
+  })
 
-  const projectConfig = config.getDefault();
-  const graphbackConfig = projectConfig.extension(graphbackExtension);
+  const projectConfig = config.getDefault()
+  const graphbackConfig = projectConfig.extension(graphbackExtension)
 
-  const modelDefs = projectConfig.loadSchemaSync(graphbackConfig.model);
+  const modelDefs = projectConfig.loadSchemaSync(graphbackConfig.model)
 
-  const db = await connectDB();
+  const db = await connectDB()
 
   const { typeDefs, resolvers, contextCreator } = buildGraphbackAPI(modelDefs, {
-    dataProviderCreator: createMongoDbProvider(db)
-  });
+    dataProviderCreator: createMongoDbProvider(db),
+  })
 
-  const apolloServer = new ApolloServer({
+  const apolloConfig: ApolloServerExpressConfig = {
     typeDefs,
     resolvers: [resolvers, noteResolvers],
-    context: contextCreator
-  });
+    context: contextCreator,
+    uploads: false,
+  }
+  const apolloServer = new ApolloServer(apolloConfig)
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({ app })
 
-  const httpServer = http.createServer(app);
-  apolloServer.installSubscriptionHandlers(httpServer);
+  const httpServer = http.createServer(app)
+  apolloServer.installSubscriptionHandlers(httpServer)
 
-  httpServer.listen({ port: 4000 }, () => {
-    console.log(`🚀  Server ready at http://localhost:4000/graphql`);
-  });
+  httpServer.listen({ port: 4000 }, () =>
+    createGraphQLWS(httpServer, apolloConfig)
+  )
 }
 
-start().catch((err: any) => console.log(err));
+start().catch((err: any) => console.log(err))
