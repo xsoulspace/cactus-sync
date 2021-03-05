df7178e0588a9145cb0a1244050d4baa5b3f5293
Anton Malofeev <arenukvern@gmail.com>
2021-03-05 21:47:30 +0300

    add: debuger config to debug tests

--- packages/cactus_sync_client/lib/src/utils/validateUuid.spec.ts
+++ packages/cactus_sync_client/lib/src/utils/validateUuid.spec.ts
@@ -3,15 +3,15 @@ describe('validateUuid', () => {
   test('Expect to be valid check = true', () => {
     const uuid = 'b82f119f-a180-4866-b6ae-91a562232746'
     const result = uuid4.valid(uuid)
-    expect(result).toEqual(true)
+    expect(result).toBeTruthy()
   })
   test('Expect to be wrong & null = false', () => {
     const uuid = 'p;a'
     const result = uuid4.valid(uuid)
-    expect(result).toEqual(false)
+    expect(result).toBeFalsy()
 
     const uuidNull = null
     const resultNull = uuid4.valid(uuidNull)
-    expect(resultNull).toEqual(false)
+    expect(resultNull).toBeFalsy()
   })
 })
