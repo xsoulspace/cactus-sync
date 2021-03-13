5fc7442e4fa6efe32c4d52a9517216385b949f15
Anton Malofeev <arenukvern@gmail.com>
2021-03-13 16:10:55 +0300

    feat(client example): add todos logic. replicating is not working

--- examples/vue-client/src/App.vue
+++ examples/vue-client/src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <button @click="findTodos">Find</button>
-  {{ foundTodo.title }}
+  <button @click="addTodo">Add</button>
+  <div>
+    <span>List Original</span>
+    <div v-for="todo in todoList" :key="todo.id">
+      <div>
+        <p>todo.title</p>
+        <p><button @click="removeTodo(todo)">x</button></p>
+      </div>
+    </div>
+  </div>
+  <div>
+    <span>List Duplicates</span>
+    <div v-for="todo in todoDuplicateList" :key="todo.id">
+      <div>todo.title</div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -18,7 +32,10 @@
     },
     setup() {
       const todoState = useTodoState()
+      const todoDubplicateState = useTodoState()
+      const counter = ref(1)
       onMounted(async () => {
+        await todoModel.startReplication()
         const todos: CreateTodoInput[] = [
           {
             _version: 1,
@@ -38,16 +55,30 @@
         }
         todoState.find()
       })
-      const foundTodo = ref<Partial<Todo>>({})
-      const findTodos = async () => {
-        const result = (
-          await todoModel.find({
-            filter: { title: { contains: 'Mars' } },
-          })
-        ).data?.items[0]
-        if (result) foundTodo.value = result
+      const addTodo = async () => {
+        const todo: CreateTodoInput = {
+          _version: 1,
+          _lastUpdatedAt: Date.now().toString(),
+          title: `New todo ${counter}`,
+        }
+        counter.value++
+        await todoState.add({
+          input: todo,
+        })
       }
-      return { findTodos, foundTodo }
+      const removeTodo = async (todo: Todo) => {
+        await todoState.remove({
+          input: {
+            id: todo.id,
+          },
+        })
+      }
+      return {
+        addTodo,
+        removeTodo,
+        todoList: todoState.list,
+        todoDuplicateList: todoDubplicateState.list,
+      }
     },
   })
 </script>
