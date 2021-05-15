84de23d5fe8cb071d1aca315a32d0e7142dedba2
Anton Malofeev <arenukvern@gmail.com>
2021-05-16 02:37:48 +0300

    fix: state update

--- dart_packages/cactus_sync_client/lib/src/abstract/cactus_model_state.dart
+++ dart_packages/cactus_sync_client/lib/src/abstract/cactus_model_state.dart
@@ -105,7 +105,10 @@ class CactusModelState<
       CactusSync.l.warning('the model is null, state will not updated');
       return;
     }
-
+    CactusSync.l.fine({
+      'maybeData is GraphbackResultList': maybeData is GraphbackResultList,
+      'maybeData runtimeType': maybeData.runtimeType
+    });
     if (maybeData is GraphbackResultList) {
       setState(maybeData.items as List<TModel?>);
     }
